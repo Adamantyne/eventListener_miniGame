2b19ed3362559ef751eab8aadcddd410dc4400db
Guilherme Alves <guialvest@gmail.com>
2022-03-07 16:37:57 -0300

    Conclusão da primeira parte

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const divPersonagem = document.querySelector(".personagem");
 const divQuarto = document.querySelector(".quarto");
 const porta = document.querySelector(".porta");
+const paredeSuperior = document.querySelector(".parede-superior");
 const pagina = document.querySelector(".pagina");
 const texto = document.querySelector(".texto");
 const informacoes = document.querySelector(".informacoes");
@@ -19,8 +20,8 @@ let numeroDoTexto=1;
 let textoContinuar = `<strong>  *Pressione enter para continuar</strong>*`;
 let texto1 ="Bem vindo! Você acabou de entrar em um mundo construindo a partir do addEventListener, então eu vou começar o apresentando a você. Neste momento, você está visualizando o quarto de Zaely (é o que eu espero '-'), nossa amiga que, gentilmente, acordou cedo pra te mostrar como tudo funciona ... como você pode notar, ela é uma garota feliz que gosta de usar roupas bem compridas (não foi por que o designer ficou com preguiça de desenhar os pés/mãos dela, confia)";
 let texto2 ="Enfim, indo direto ao assunto, existem várias ações que podem ser executadas por você, usuário, como clicar uma tecla no seu teclado, clicar com o mouse em algum elemento da tela e etc. Porém, para que esse evento execute alguma ação, ele deve ser registrado. Para isso, precisamos associar esse evento a um “event handler”, ou melhor, uma manipulador de eventos, sendo ele um bloco de código (geralmente uma função JavaScript definida pelo programador). Essa associação poderá ser feita pela propriedade do próprio objeto direcionando para a ação executada ou pelo método eventListener.";
-let texto3 = "Por exemplo, vamos supor que você queira criar uma div que, ao ser clicada, ela mude sua cor. A primeira coisa a se fazer é capturar essa div no js (com o querySelector, por exemplo). Caso você queira fazer com a propriedade do próprio objeto, basta você registrar essa propriedade nele. Nesse nosso exemplo da div, utilizaríamos o “onclick” (objeto.onclick= “event handler” ) sendo o event handler a função a ser executada, alterando a cor da div.";
-let texto4 ="Caso você queira fazer isso com o método addEventListener(), você deverá associa-lo ao objeto e, a priori, passar dois parâmetros. Você precisará indicar o evento e também a função a ser executada (nesse caso, trabalha como uma função de call-back).Voltando ao exemplo da div que muda de cor, ficaria: objeto.addEventListener(“click”, função());, sendo a função passada como parâmetro responsável por alterar a cor da div.";
+let texto3 = "Por exemplo, vamos supor que você queira criar uma div que, ao ser clicada, ela mude sua cor. A primeira coisa a se fazer é capturar essa div no js (com o querySelector, por exemplo). Caso você queira fazer com a propriedade do próprio objeto, basta você registrar o “onclick” (objeto.onclick= “função que altera a cor” )";
+let texto4 ="Caso você queira fazer isso com o método addEventListener(), você deverá associa-lo ao objeto e, a priori, passar dois parâmetros. Você precisará indicar o evento e também o listener/função a ser executada (nesse caso, trabalha como uma função de call-back e possui como retorno em seu parâmetro o objeto 'Event'que carrega informações sobre o evento ocorrido).Voltando ao exemplo da div que muda de cor, ficaria: objeto.addEventListener(“click”, função(Event));, sendo a função passada como parâmetro responsável por alterar a cor da div.";
 let texto5 ="Agora vamos ver isso tudo ocorrendo na prática! Neste momento, Zaely está esperando você dizer o que fazer para ela. Para isso, o eventListener ira avisa-la sobre a sua decisão. Tendo o DOM como objeto, está registrado no eventListener o evento de pressionar para duas teclas: “A”/”D”.(document. addEventListener('keypress',função())). Digite “A” para zaely andar para a esquerda e “D” para direita.";
 let texto6 = "Para explorar o lado de fora, você precisará primeiro abrir a porta. Sendo a porta o objeto desta vez, o eventListener registrou o evento de click, acionando a função que abre a porta (porta. addEventListener(“click”, função()). Clique na porta para abri-la.";
 let texto7 ="Bom, é isso. Espero que Zaely tenha te ajudado a explorar um pouco desse maravilhoso mundo dos eventos(caso não tenha, foi culpa dela em ...). Mas não pare por aqui, ainda tem muito mais a ser explorado! Boa sorte em seus estudos XD.";
@@ -112,6 +113,7 @@ function adicionandoEventoAbrirPorta(){
         quarto = document.querySelector(".quarto img");
         quarto.style.left=`${posicao}px`;
         pagina.style.backgroundColor=`white`;
+        paredeSuperior.classList.remove("escondido");
     })
 }
 document.addEventListener("keyup", ()=>{
@@ -134,6 +136,7 @@ function movendoMapa(valor){
     bloqueandoPassagem(posicao);
     quarto.style.left=`${posicao}px`;
     porta.style.left=`${posicao+850}px`;
+    paredeSuperior.style.left=`${posicao+940}px`;
     cenario.style.left=`${posicao+973}px`
     console.log(posicao);
 }
@@ -146,4 +149,9 @@ function bloqueandoPassagem(posicaoBloqueada){
         posicao=-630;
     }
 }
-exibindoInformacoes(numeroDoTexto);
+setTimeout(iniciar,3500);
+function iniciar(){
+    let abertura = document.querySelector(".abertura");
+    abertura.classList.add("escondido");
+    exibindoInformacoes(numeroDoTexto);
+}
